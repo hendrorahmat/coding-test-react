28f5eb1a676f1703a95b0cf2ff6ce0106e11104a
Toan Nguyen <toan210597ntu@gmail.com>
2024-05-21 14:01:25 +0700

    add new react test version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import Logo from "/logo.svg";
 import "./App.css";
 
+/**
+ * TODO:
+ * 1. Create Custom hook is call useFriends with these features:
+ *   - Fetch data from endpoint
+ *   - Pagination with data from endpoint
+ *   - Support search via api
+ *   => Here is 2 endpoint you can use:
+ *   - https://dummyjson.com/users?limit=5&skip=0&select=id,firstName,lastName,maidenName,age
+ *   - https://dummyjson.com/users/search?q=ann&limit=5&skip=0&select=id,firstName,lastName,maidenName,age
+ * 2. Create Friends component and apply above custom hook:
+ *   - Show list of friends with 2 button Next, Previous for pagination
+ *   - Show search input for searching friends
+ *
+ */
+
 const App = () => {
   return (
     <>
@@ -9,13 +24,8 @@ const App = () => {
       </div>
       <h1>Social Network App</h1>
       <div>
-        <h3>Maybe friends?</h3>
-        {/*
-          TODO: show people who may be friends
-
-          you can get date from the below endpoint:
-          https://private-e843bc-betamindhiring.apiary-mock.com/users/1/maybe_friends
-        */}
+        <h3>friends?</h3>
+        {/* Please add Friends Component here */}
       </div>
     </>
   );
